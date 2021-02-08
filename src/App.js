85112211227f1167a997e54b6d2ed865030ccff8
Egor18032019 <goro4@mail.ru>
2021-02-08 13:34:55 +0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import logo from './assets/logo.png';
+import './App.css';
+import FirstGame from "./scenes/FirstGame.js"
+
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        {/* <a
+          className="App-link"
+          href="https://photonstorm.github.io/phaser3-docs/Phaser.GameObjects.GameObject.html"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn Phaser
+        </a> */}
+      </header>
+        <main>
+        {/* <FirstGame /> */}
+        </main>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/scenes/FirstGame.js
@@ -0,0 +1,47 @@
+
+import Phaser from 'phaser';
+import React, { useState } from 'react'
+import { IonPhaser } from '@ion-phaser/react'
+
+import WorldScene from './WorldScene.js';
+import BootScene from './BootScene.js';
+ 
+   const mainGame = {
+      type: Phaser.AUTO,
+      // backgroundColor: '#555555',
+      // parent: 'phaser-example',
+      parent: 'content',
+      width: 320,
+      height: 240,
+      zoom: 2,
+      pixelArt: true, //чтобы не было размытия текстур при масштабировании  
+      physics: {
+        default: 'arcade',
+        arcade: {
+          gravity: { y: 300 },  
+          debug: false,
+        },
+      },
+      scene: [
+        BootScene,WorldScene
+         ],
+    }
+
+  
+ 
+const FirstGame = () => {
+  const [gameState] = useState({
+      initialize: true,
+      game: mainGame
+  });
+  const { initialize, game } = gameState;
+
+  return (
+      <div className='game-wrapper'>
+          <div className='wrapper'>
+              <IonPhaser game={game} initialize={initialize} />
+          </div>
+      </div>
+  )
+};
+export default FirstGame;
